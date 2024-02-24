f1fa4a5f953c1154fdb1156570c5490dae5714d3
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-02-24 20:14:21 +0000

    Employee class

--- starter/index.js
+++ starter/index.js
@@ -1,8 +1,17 @@
+//Team profile Generator
+
+//Local JS files 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+//requre function to load inquirer data 
 const inquirer = require("inquirer");
+
+//require function to load path data 
 const path = require("path");
+
+// Read a file in Node.js
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
