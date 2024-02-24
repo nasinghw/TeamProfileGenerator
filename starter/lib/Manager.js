becac221e700e34a29742b3d998424c1d91771da
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-02-24 20:31:26 +0000

    Manager class

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,3 +1,12 @@
 // Code to define and export the Manager class.
 
 //This class is inherited from Employee.
+
+const Employee = require("./Employee");
+
+class Manager extends Employee{
+    constructor(name, id, email, officeNumber){
+        super(name,id,email);
+        this.officeNumber = officeNumber;
+        this.title = "Manager"
+}}
