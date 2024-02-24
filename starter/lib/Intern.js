b32e20290e4dc004f9d226d6f8de83a32e6e922b
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-02-24 20:24:57 +0000

    intern class

--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,3 +1,11 @@
 // Code to define Intern class.
 
 //class inherited from Employee.
+
+const Employee = require('./Employee');
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
