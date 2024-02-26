265d99eb2f5d41b56987c819ca2e076846f8e242
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-02-26 20:34:50 +0000

    User questions

--- starter/index.js
+++ starter/index.js
@@ -1,81 +1,105 @@
-//Team profile Generator
+// Packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateHTML = require('./src/generateHtmlPage');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
 
-//Local JS files 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-
-//requre function to load inquirer data 
-const inquirer = require("inquirer");
-
-//require function to load path data 
-const path = require("path");
-
-// Read a file in Node.js
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./src/page-template.js");
-
-
-// Code to gather information about the development team members, and render the HTML file.
-
-//USer questions
-
-class Prompt {
-    constructor(){
+// Class containing all questions
+class Prompt{
+    constructor() {
         this.teamArray = [];
     }
 
-    getTeamArray(){
-        return this.teamArray
+    /**
+     * @returns the array of all Employee objects
+     */
+
+    getTeamArray() {
+        return this.teamArray;
     }
 
+// User Questions
 questions() {
-    inquirer.prompt({
-        type: 'list',
-        name: 'employeeTitle',
-        message: "Select employee title from list to add",
-        selection: ['Manager',  'Engineer', 'Intern', 'I have selected my team'] 
+    inquirer.prompt(
+    {
+     type: 'list',
+     name: 'employeeTitle',
+     message: "Choose team employee you would like to add ?",
+     options: ['Manager', 'Engineer', 'Intern', 'I finished adding my team']
+    })
+    .then(({employeeTitle}) => {
+        if(employeeTitle === 'Manager'){
+        inquirer.prompt([
+    {
+     type: 'input',
+     name: 'name',
+     message: "Please provide manager's name",
+     
+    },
+    {
+        type: 'number',
+        name: 'id',
+        message: "Please provide manager's employee id",
+        
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please provide manager's email",
+        
+    },
+    {
+        type: 'number',
+        name: 'officeNumber',
+        message: "Please provide manager's office number",
+        
+    },
+    ])
 
-    }).then(({employeeTitle}) => {
-        if (employeeTitle === 'Manager') {
+    // Pushes Manager data into teamArray
+    .then( (templateData) => {
+        const newManager = new Manager(templateData.name, templateData.id, templateData.email, templateData.officeNumber)
+        this.teamArray.push(newManager);
+        // Sends user back to menu
+        this.questions();
+    });
+
+    } else if (employeeTitle === 'Engineer') {
             inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'name',
-                    message: "Please provide manager's name",
-                    
-                },
-                {
-                    type: 'number',
-                    name: 'id',
-                    message: "Please provide manager's employee id",
-                   
-                },
+                    {
+                     type: 'input',
+                     name: 'name',
+                     message: "Please provide engineer's name",
+                     
+                    },
+                    {
+                     type: 'number',
+                     name: 'id',
+                     message: "Please provide engineer's employee id",
+                     
+                    },
+                    {
+                     type: 'input',
+                     name: 'email',
+                     message: "Please provide engineer's email",
+                     
+                    },
+                    {
+                     type: 'input',
+                     name: 'github',
+                     message: "Please provide engineer's github username",
+                     
+                    }
 
-                {
-                    type: 'input',
-                    name: 'email',
-                    message: "Please provide manager's email",
-                    
-                },
-                {
-                    type: 'number',
-                    name: 'officeNumber',
-                    message: "Please enter the manager's office number",
-                    
-                },
-                
-                // Store Engineer data into teamArray
-            ]).then( templateData => {
-                const newEngineer = new Engineer(templateData.name, templateData.id, templateData.email, templateData.github);
-                this.teamArray.push(newEngineer);
-                // Return user back to menu
-                this.questions();
-            });
+                // Pushes Engineer data into teamArray
+                ]).then( templateData => {
+                    const newEngineer = new Engineer(templateData.name, templateData.id, templateData.email, templateData.github);
+                    this.teamArray.push(newEngineer);
+                    // Sends user back to menu
+                    this.questions();
+                });
 
         } else if (employeeTitle === 'Intern') {
             inquirer.prompt([
@@ -83,35 +107,28 @@ questions() {
                  type: 'input',
                  name: 'name',
                  message: "Please provide intern's name",
+                 
+                },
+                {
+                 type: 'number',
+                 name: 'id',
+                 message: "Please provide intern's employee id",
+                 
+                },
+                {
+                 type: 'input',
+                 name: 'email',
+                 message: "Please provide intern's email",
+                 
+                },
+                {
+                 type: 'input',
+                 name: 'school',
+                 message: "Please provide intern's school name",
                 
-                },
-                {
-                    type: 'number',
-                    name: 'id',
-                    message: "Please provide intern's employee id",
-                   
-                },
-                {
-                    type: 'input',
-                    name: 'email',
-                    message: "Please provide intern's email",
-                    
-                },
-                {
-                    type: 'input',
-                    name: 'school',
-                    message: "Please enter the intern's school name",
-                    
                 }
 
-                // Store Manager data into teamArray
-                .then( (templateData) => {
-                    const newManager = new Manager(templateData.name, templateData.id, templateData.email, templateData.officeNumber)
-                    this.teamArray.push(newManager);
-                    // Sends user back to menu
-                    this.questions();
-                });
-                // Pushes Intern data into teamArray
+            // Pushes Intern data into teamArray
             ]).then( templateData => {
                 const newIntern = new Intern(templateData.name, templateData.id, templateData.email, templateData.school);
                 this.teamArray.push(newIntern);
@@ -119,20 +136,21 @@ questions() {
                 this.questions();
             });
 
-            } else if (employeeTitle === 'I finished entering my team info') {
+        } else if (employeeTitle === 'I finished entering my team info') {
             //function that writes the html file in the dist folder
-                const pagehtml = generateHTML(this.getTeamArray());
-                fs.writeFile('./dist/index.html', pagehtml, err => {
+            const pagehtml = generateHTML(this.getTeamArray());
+            fs.writeFile('./dist/index.html', pagehtml, err => {
                 if (err) throw new Error(err);
 
                 console.log('Page created! Check out index.html in the dist/ folder to see it!');
             });
-            }
-        });
-    }
+        }
+    });
+
+}
 };
 
-// Prompts in CLI
+// Activates prompts in CLI
 const prompt = new Prompt();
 prompt.questions();
 
